f5b4092d9f8da11d6577cedf21918956645aeccc
Sergio Espinosa <sjespino@outlook.com>
2022-06-08 14:41:04 -0400

    Archivos iniciales
    bases con headers, ícono editado por mi
    Vista inicio para subscribir, carrusel funcional pero sin imágenes.
    Vista requerimientos con tabla (va a cambiar a tarjetas).
    
    en proceso: tarjetas de requerimientos, vista cliente.

---
+++ app/Views/newViews/inicio.php
@@ -0,0 +1,97 @@
+<?php
+    include("baseGuest.php");
+?>    
+        
+<div class="container">
+    <div class="row">
+        <div class="col-sm-3">
+            <div class="row">
+                <strong>Nucleova para Empresas</Strong>
+            </div>
+            &nbsp 
+            <div class="row">
+                <p id="startContent" align="justify">Acceso rápido y sencillo a través de la aplicación web para establecer los requerimientos de los servicios solicitados con indicadores de gestión que te ayudarán a medir constantemente la calidad de los servicios y un acceso total a la red de proveedores de nucleova</p>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div id="carouselId" class="carousel slide" data-bs-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-bs-target="#carouselId" data-bs-slide-to="0" class="active" aria-current="true" aria-label="First slide"></li>
+                    <li data-bs-target="#carouselId" data-bs-slide-to="1" aria-label="Second slide"></li>
+                    <li data-bs-target="#carouselId" data-bs-slide-to="2" aria-label="Third slide"></li>
+                </ol>
+                <div class="carousel-inner" role="listbox">
+                    <div class="carousel-item active">
+                        <img src="" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="" alt="Third slide">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselId" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Anterior</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselId" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Siguiente</span>
+                  </button>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="row">
+                <strong>Caracteristicas</strong>
+            </div>
+            <div class="row">
+                <table class="table" id="tablaCaracteristicas">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th></th>                       
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td scope="row">Interfaz de administración</td> 
+                            <td><span class="ticket"></span></td>                     
+                        </tr>
+                        <tr>
+                            <td scope="row">Historial de servicios </td> 
+                            <td><span class="ticket"></span></td>                      
+                        </tr>
+                        <tr>
+                            <td scope="row"> control de servicios </td>   
+                            <td><span class="ticket"></span></td>                     
+                        </tr>
+                        <tr>
+                            <td scope="row">Interfaz de administración </td>   
+                            <td><span class="ticket"></span></td>                     
+                        </tr>
+                        <tr>
+                            <td scope="row">Interfaz de administración</td>  
+                            <td><span class="ticket"></span></td>                      
+                        </tr>
+                    </tbody>
+                </table>
+           </div>
+           <div class="row">
+               <p>$5.000</p>
+           </div>
+           <div class="row">
+               <button type="button" class="btn btn-primary">Subscribirme</button>
+           </div>
+            
+                
+                
+                
+            
+            
+        </div>
+    </div>
+</div>
+        
+
+ 
---
+++ app/Views/newViews/requerimientos.php
@@ -0,0 +1,36 @@
+<?php
+    include("baseLogged.php");
+?>    
+        
+<div class="container">
+    <div class="row">
+        <div class="col-sm-3">
+            <button type="button" name="" id="" class="btn btn-primary" style="background-color: #8A4397; border-color: #8A4397;" >Nuevo Requerimiento <span class="badge bg-light text-dark">+</span></button>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Resumen</th>
+                        <th>Problemática</th>
+                        <th>Respuesta</th>
+                        <th>Opciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td scope="row"></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+        
+
+ 
