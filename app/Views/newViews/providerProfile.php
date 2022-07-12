91f17d4992c960a5316f53601d9b76b02a8fae8d
Sergio Espinosa <sjespino@outlook.com>
2022-07-12 04:53:49 -0400

    vistas finales
    vistas de inicio y buscador
    correccion modales
    agreados modales pov proveedor
    readme

---
+++ app/Views/newViews/providerProfile.php
@@ -0,0 +1,67 @@
+<?php
+    include("./userViews/baseLoggedUser.php");
+?>   
+<div class="container cont60">
+    
+	<div class="card">
+		
+		<div class="card-body">			
+            <div class="row bottom5">
+                <div class="col-md-5">
+                    <div class="row">
+                        <div class="col">
+                            <img class="rounded img-fluid w-100 fit-cover bottom20" style="min-height: 300px;" src="" />
+                        </div>
+                    </div>            
+                    <div class="row">
+                        <div class="row">
+                            <div class="col-12 d-grid gap-2">
+                                <button type="button" class="btn btn-primary bottom15 btn-purple text-start"><span class="fa-regular fa-comment"></span> Chat con el proveedor <!--span class="badge rounded-pill bg-danger float-sm-end">5</span--></button>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12 d-grid gap-2">  
+                                <button type="button" class="btn btn-primary bottom15 btn-success text-start"><span class="fa-regular fa-user-group"></span> Enviar Solicitud de contacto</button>
+                            </div>                            
+                        </div>
+                    </div>  
+                </div>
+                <div class="col-md-7">            
+                    <div class="card bottom30">
+                        <div class="card-header text-center">
+                            <h4>Nombre Proveedor</h4>
+                            <div class="row">                                
+                                <div class="col">
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span>
+                                </div>
+                            </div>
+                        </div>                
+                    </div>
+                    <div class="card bottom30">                
+                        <div class="card-body"> 
+                            <p class="card-text">Nullam id dolor id nibh ultricies vehicullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p>
+                            <ul class="list-group text-center">
+                                <li class="list-group-item"><span class="fa-regular fa-phone"></span><strong> +56912345678</strong></li>
+                                <li class="list-group-item"><span class="fa-regular fa-envelope"></span><strong> email@proveedor.com</strong></li>
+                                <li class="list-group-item">
+                                    <div class="row">
+                                        <div class="col">
+                                            <a href="#" class="fa-brands left5 right5 fa-social fa-facebook"></a>
+                                            <a href="#" class="fa-brands left5 right5 fa-social fa-twitter"></a>
+                                            <a href="#" class="fa-brands left5 right5 fa-social fa-instagram"></a>
+                                            <a href="#" class="fa-brands left5 right5 fa-social fa-linkedin"></a>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>                
+                    </div>                         
+                </div>
+            </div>
+        </div>
+	</div>    
+</div>
