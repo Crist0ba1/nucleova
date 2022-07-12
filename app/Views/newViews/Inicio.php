1596d4a3d900ee50dbbb0a19aeb2b98c185bf73f
Sergio Espinosa <sjespino@outlook.com>
2022-07-12 11:54:55 -0400

    correcciones finales landing
    agrega buscador base y filtros en resultados

--- app/Views/newViews/Inicio.php
+++ app/Views/newViews/Inicio.php
@@ -2,130 +2,19 @@
     include("baseGuest.php");
 ?> 
 
-<div class="container">
+<div class="container cont60 text-center">
     <div class="row">
-        <div class="col-md-3">            
-            <form class="d-flex bottom15">
-                <input class="form-control right15" type="search" placeholder="Buscar" aria-label="Search">
-                <button class="btn btn-purple text-light" type="submit"><span class="fa-regular fa-search"></span></button>
+        <div class="col-xl-9 mx-auto position-relative">
+            <h3 class="mb-5">Buscar proveedores</h3>
+        </div>
+        <div class="col-md-10 col-lg-8 col-xl-7 mx-auto position-relative">
+            <form>
+                <div class="row">
+                    <div class="col-12 col-md-9 mb-2 mb-md-0">
+                        <input class="form-control form-control-lg" type="text" placeholder="ingresa el campo de búsqueda"/></div>
+                    <div class="col-12 col-md-3"><button class="btn btn-primary btn-lg" type="submit"><span class="fa-regular fa-search"></span> Buscar</button></div>
+                </div>
             </form>
-            <p class="bottom15"><span class="fa-regular fa-filter"></span> Filtrar por</p>            
-            <div id="accordion-1" class="accordion " role="tablist">
-                <div class="accordion-item">
-                    <h2 class="accordion-header" role="tab">
-                        <button class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#accordion-1 .item-1" aria-expanded="false" aria-controls="accordion-1 .item-1">Región</button>
-                    </h2>
-                    <div class="accordion-collapse collapse item-1" role="tabpanel" data-bs-parent="#accordion-1">
-                        <div class="accordion-body" style="padding:0;">
-                            <ul class="list-group-flush">
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Arica y Parinacota</a>
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Tarapacá</a>
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Antofagasta</a>
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Atacama</a>
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Coquimbo</a>
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Valparaíso</a>
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Metropolitana de Santiago</a>
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Bernardo O'Higgins</a>
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Maule</a>
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Ñuble</a>
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Biobío</a>
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Araucanía</a>
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Los Ríos</a>
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Los Lagos</a>
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Aysén</a>
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Magallanes y la Antártica</a>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="accordion-item">
-                    <h2 class="accordion-header" role="tab">
-                        <button class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#accordion-1 .item-2" aria-expanded="false" aria-controls="accordion-1 .item-2">Ciudad</button>
-                    </h2>
-                    <div class="accordion-collapse collapse item-2" role="tabpanel" data-bs-parent="#accordion-1">
-                        <div class="accordion-body" style="padding:0;">
-                            <ul class="list-group-flush"> 
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Ciudad 1</a>
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Ciudad 2</a>
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Ciudad 3</a>
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Ciudad 4</a>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="accordion-item">
-                    <h2 class="accordion-header" role="tab"><button class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#accordion-1 .item-3" aria-expanded="false" aria-controls="accordion-1 .item-3">Categoría</button></h2>
-                    <div class="accordion-collapse collapse item-3" role="tabpanel" data-bs-parent="#accordion-1">
-                        <div class="accordion-body">
-                            <ul class="list-group-flush">
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Categoría 1</a>
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Categoría 2</a>
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Categoría 3</a>
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Categoría 4</a>
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Categoría 5</a>
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Categoría 6</a>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="accordion-item">
-                    <h2 class="accordion-header" role="tab"><button class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#accordion-1 .item-4" aria-expanded="false" aria-controls="accordion-1 .item-3">Subcategoría</button></h2>
-                    <div class="accordion-collapse collapse item-4" role="tabpanel" data-bs-parent="#accordion-1">
-                        <div class="accordion-body">
-                            <ul class="list-group-flush">
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Subcategoría 1</a>
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Subcategoría 2</a>
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Subcategoría 3</a>
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Subcategoría 4</a>
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Subcategoría 5</a>
-                                <a href="#" class="list-group-item list-group-item-action "><span class="fa-solid fa-caret-right"></span> Subcategoría 6</a>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="text-center">
-                    <button class="btn btn-purple text-light top15" type="submit"><span class="fa-regular fa-filter-circle-xmark"></span> Quitar Filtros</button>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-9">
-            <div class="d-flex flex-row-reverse bottom15">                              
-                <div class="dropdown right5">
-                    <a class="btn btn-light dropdown-toggle" href="#" role="button" id="dropOrderBy" data-bs-toggle="dropdown" aria-expanded="false">Ordenar Por</a>
-                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                        <li><a class="dropdown-item" href="#">Nombre</a></li>
-                        <li><a class="dropdown-item" href="#">Puntuación</a></li>
-                        <li><a class="dropdown-item" href="#">Fecha de ingreso</a></li>
-                    </ul>
-                </div> 
-            </div>
-            <div class="container-fluid justify-content-end">
-                <div class="card-deck d-flex justify-content-end">
-                    <?php
-                        for($i = 0; $i <4; $i++){
-                            include("searchResultCardGrid.php");
-                        }
-                    ?>
-                </div>
-                <div class="card-deck d-flex justify-content-end">
-                    <?php
-                        for($i = 0; $i <4; $i++){
-                            include("searchResultCardGrid.php");
-                        }
-                    ?>
-                </div>
-                
-            </div>
-            <nav>
-                <ul class="pagination justify-content-end">
-                    <li class="page-item"><a class="page-link" href="#">Anterior</a></li>
-                    <li class="page-item"><a class="page-link" href="#">1</a></li>
-                    <li class="page-item"><a class="page-link" href="#">2</a></li>
-                    <li class="page-item"><a class="page-link" href="#">3</a></li>
-                    <li class="page-item"><a class="page-link" href="#">Siguiente</a></li>
-                </ul>
-            </nav>
-        </div>
+        </div>        
     </div>
-</div>
+</header>
--- app/Views/newViews/plans.php
+++ app/Views/newViews/plans.php
@@ -7,49 +7,69 @@
     <div class="row mb-5">
         <div class="col-md-8 col-xl-6 text-center mx-auto">
             <h2>Planes</h2>
-            <p class="w-lg-50">Elige el plan que mejor te acomode para disfrutar de Nucleova</p>
+            <p class="w-lg-50">Elige el plan que mejor te acomode para disfrutar de Nucleova </p>
+            <strong>1 mes gratis por tiempo limitado contratando cualquier plan</strong>
         </div>
     </div>
     <div class="row gy-4 gy-xl-0 row-cols-1 row-cols-md-2 row-cols-xl-3 d-xl-flex align-items-xl-center gutter-y">
-        <div class="col">
+        <div class="col-md-3">
             <div class="card">
                 <div class="card-body text-center p-4">
                     <h6 class="text-uppercase text-muted card-subtitle">Mensual</h6>
-                    <h4 class="display-4 fw-bold card-title" style="font-size:3vw;">$5.000</h4>
+                    <h4 class="display-4 fw-bold card-title" style="font-size:2vw;">$8.000</h4>
                 </div>
                 <div class="card-footer p-4">
                     <a class="btn btn-blue text-white d-block w-100" role="button" href="#">Contratar Ahora</a>
                 </div>
             </div>
         </div>
-        <div class="col">
+        <div class="col-md-3">
             <div class="card">
                 <div class="card-body text-center p-4">
-                    <h6 class="text-uppercase text-muted card-subtitle">Semestral</h6>
-                    <h4 class="display-4 fw-bold card-title font-scale-price" style="font-size:3vw;">$28.800</h4>
+                    <h6 class="text-uppercase text-muted card-subtitle">Trimestral</h6>
+                    <h4 class="display-4 fw-bold card-title" style="font-size:2vw;">$21.900</h4>
                 </div>
                 <div class="card-footer p-4">
                     <div>
                         <ul class="list-unstyled">
-                            <li><span class="fa-regular fa-circle-dot"></span>  Nucleova pro por $4.800 mensual.</li>
+                            <li><span class="fa-regular fa-circle-dot"></span>  Nucleova pro por $7.300 mensual.</li>
                             
                         </ul>
-                    </div><a class="btn btn-blue text-white d-block w-100" role="button" href="#">Contratar Ahora</a>
+                    </div>
+                    <a class="btn btn-blue text-white d-block w-100" role="button" href="#">Contratar Ahora</a>
                 </div>
             </div>
         </div>
-        <div class="col">
-            <div class="card border-blue border-2">
-                <div class="card-body text-center p-4"><span class="badge rounded-pill bg-blue position-absolute top-0 start-50 translate-middle text-uppercase">Mejor precio</span>
-                    <h6 class="text-uppercase text-muted card-subtitle">Anual</h6>
-                    <h4 class="display-4 fw-bold card-title font-scale-price" style="font-size:3vw;">$54.000</h4>
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body text-center p-4">
+                    <h6 class="text-uppercase text-muted card-subtitle">Semestral</h6>
+                    <h4 class="display-4 fw-bold card-title font-scale-price" style="font-size:2vw;">$42.000</h4>
                 </div>
                 <div class="card-footer p-4">
                     <div>
                         <ul class="list-unstyled">
-                            <li><span class="fa-regular fa-circle-dot"></span> Nucleova pro por $4.500 mensual.</li>                            
+                            <li><span class="fa-regular fa-circle-dot"></span>  Nucleova pro por $7.000 mensual.</li>
+                            
                         </ul>
-                    </div><a class="btn btn-blue text-white d-block w-100" role="button" href="#">Contratar Ahora</a>
+                    </div>
+                    <a class="btn btn-blue text-white d-block w-100" role="button" href="#">Contratar Ahora</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card border-blue border-2">
+                <div class="card-body text-center p-4"><span class="badge rounded-pill bg-blue position-absolute top-0 start-50 translate-middle text-uppercase">Mejor precio</span>
+                    <h6 class="text-uppercase text-muted card-subtitle">Anual</h6>
+                    <h4 class="display-4 fw-bold card-title font-scale-price" style="font-size:2vw;">$81.000</h4>
+                </div>
+                <div class="card-footer p-4">
+                    <div>
+                        <ul class="list-unstyled">
+                            <li><span class="fa-regular fa-circle-dot"></span> Nucleova pro por $6.750 mensual.</li>                            
+                        </ul>
+                    </div>
+                    <a class="btn btn-blue text-white d-block w-100" role="button" href="#">Contratar Ahora</a>
                 </div>
             </div>
         </div>
